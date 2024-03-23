65ba45d56292f1b18bdb7734bce330c2a7686cac
SardarNazeer <sardarnazeer1108@gmail.com>
2024-03-23 08:02:18 +0500

    day 1 of dom 2 qs solved

---
+++ main.js
@@ -0,0 +1,15 @@
+// q1) Create a h2 heading wuth text - "Hello Javascript".append "from apna college students" to this text using js 
+
+// let heading = document.querySelector("h2");
+
+// console.dir(heading.innerText);
+
+// heading.innerText = heading.innerText + " from sardar nazeer";
+
+// q2) create 3 div with common class name - 'box' acces them & add some unique text to each of them 
+
+let divs = document.querySelectorAll(".box");
+
+divs[0].innerText = "js is hard val 1";
+divs[1].innerText = "js is hard but will make it easy val 2";
+divs[2].innerText = "js is hard but will make it easy by practicing daily val 3";
